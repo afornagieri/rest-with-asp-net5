42a9c6d093d08bcb3a2773de5ac2d2fd4a54944b
afornagieri <fornagieri.artur@gmail.com>
2022-04-15 14:09:33 -0300

    feat: PersonController

---
+++ RestWithASPNET/RestWithASPNET/Services/IPersonService.cs
@@ -0,0 +1,17 @@
+﻿using RestWithASPNET.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person Update(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        void Delete(long id);
+    }
+}
---
+++ RestWithASPNET/RestWithASPNET/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,68 @@
+﻿using RestWithASPNET.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+        }
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirsName = "YourName",
+                LastName = "YourLastName",
+                Address = "Some address",
+                Gender = "Male"
+            };
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirsName = "YourName" + i,
+                LastName = "YourLastName" + i,
+                Address = "Some address" + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
